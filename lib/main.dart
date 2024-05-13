3a77ead72b83dba3e22a98e7441ae722c80718db
abiharinesh <abiharinesh@gmail.com>
2024-05-13 15:47:58 +0530

    Implement the user screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/provider/dark_theme_provider.dart';
-import 'package:grocery_app/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'consts/theme_data.dart';
